6ec0ac796001ae6581016642d8d94e53512e19f3
Alex <alexander.stuckenholz@gmail.com>
2022-08-31 17:24:40 +0200

    Working on chapter 10

--- 10/Program.cs
+++ 10/Program.cs
@@ -1,5 +1,22 @@
 ﻿public class Program
 {
+    public static void ZufallszahlenInArray()
+    {
+        Random rnd = new Random();
+        int[] zahlen = new int[100];
+
+        for (int i=0; i<100; i++)
+        {
+            zahlen[i] = rnd.Next(1, 7);
+        }
+
+        int summe = 0;
+        for (int i=0; i<100; i++)
+        {
+            summe += zahlen[i];
+        }
+    }
+
     public static void Main(string[] args)
     {
         
